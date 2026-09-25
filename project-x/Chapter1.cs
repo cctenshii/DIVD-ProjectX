0af437644f09bb486f1161e41e63a5dbc9d8101e
Trevino Rizky Sunarja <145679289+TrevinoRizkySunarja@users.noreply.github.com>
2026-09-25 12:48:33 +0200

    Update Chapter1.cs

--- project-x/Chapter1.cs
+++ project-x/Chapter1.cs
@@ -1,15 +1,34 @@
 using Godot;
-using System;
 
 public partial class Chapter1 : Node2D
 {
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{GD.Print("Chapter 1 gestart");
-	}
+	private Vector2 _playerPosition = new(400, 300);
+	private float _time;
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		// Beweeg het blauwe blok met de pijltjestoetsen.
+		Vector2 direction = Input.GetVector(
+			"ui_left", "ui_right", "ui_up", "ui_down"
+		);
+
+		_playerPosition += direction * 250f * (float)delta;
+
+		// Laat het rode object vanzelf heen en weer bewegen.
+		_time += (float)delta;
+		QueueRedraw();
+	}
+
+	public override void _Draw()
+	{
+		DrawRect(new Rect2(0, 0, 900, 600), new Color("#18202e"));
+
+		DrawRect(
+			new Rect2(_playerPosition, new Vector2(40, 40)),
+			new Color("#35bfff")
+		);
+
+		float enemyX = 650f + Mathf.Sin(_time * 2f) * 120f;
+		DrawCircle(new Vector2(enemyX, 300), 22f, new Color("#ff5252"));
 	}
 }
