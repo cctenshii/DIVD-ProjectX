59f58c5d2b10ed15639af580f949473dc85b0dda
Trevino Rizky Sunarja <145679289+TrevinoRizkySunarja@users.noreply.github.com>
2026-09-25 12:46:49 +0200

    Start

---
+++ project-x/Chapter1.cs
@@ -0,0 +1,15 @@
+using Godot;
+using System;
+
+public partial class Chapter1 : Node2D
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{GD.Print("Chapter 1 gestart");
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+}
